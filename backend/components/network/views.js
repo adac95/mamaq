e9a0e7d488c7aef14778a2c2cdcf7a6844ab8d90
Andre del Alcazar <adac9525@gmail.com>
2021-08-15 01:08:56 -0500

    arreglando la direccion destino del input _next en el form index.ejs

--- backend/components/network/views.js
+++ backend/components/network/views.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router()
 const Model = require('../api-products/model')
+const {port} = require('../../config/index')
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { port })
 })
 
 router.get('/reservas', (req, res) => {
