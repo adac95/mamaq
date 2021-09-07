552394dd4cc868287c05788cddd3b20aa6cd3f0b
Andre del Alcazar <adac9525@gmail.com>
2021-09-06 22:47:08 -0500

    separando por funciones get.mjs de create-products

--- fronted/public/js/create-products/get.mjs
+++ fronted/public/js/create-products/get.mjs
@@ -10,28 +10,9 @@ export default async function getAll(url, table, template, fragment) {
         if (error) throw { error: `error al solicitar productos: ${error}` }
 
         body.forEach(el => {
-            template.querySelector(".item-name").textContent = el.name;
-            template.querySelector(".item-price").textContent = el.price;
-            template.querySelector(".item-category").textContent = el.category;
-            template.querySelector(".item-description").textContent = el.description;
-            // DANDOLE EL SRC Y ALT A LAS IMAGENES (SI ES QUE EXISTEN)
-            if (el.imagen.path) {
-                template.querySelector(".item-image__img").src = `../${el.imagen.path}`
-                template.querySelector(".item-image__img").alt = el.imagen.originalname;
-            } else {
-                template.querySelector(".item-image__img").src = ""
-                template.querySelector(".item-image__img").alt = "sin imagen"
-            }
-            template.querySelector(".edit-btn").dataset._id = el._id;
-            template.querySelector(".item-tr-fetch").dataset._id = el._id;
-            template.querySelector(".item-tr-fetch").dataset.name = el.name;
-            template.querySelector(".item-tr-fetch").dataset.price = el.price;
-            template.querySelector(".item-tr-fetch").dataset.category = el.category;
-            template.querySelector(".item-tr-fetch").dataset.description = el.description;
-            template.querySelector(".delete-btn").dataset._id = el._id;
-
-            let clone = document.importNode(template, true);
-            fragment.appendChild(clone);
+            setTextContent(el,template)
+            setDataset(el, template)
+            cloneTemplate(fragment, template)
         });
 
         table.querySelector("tbody").appendChild(fragment);
@@ -42,4 +23,34 @@ export default async function getAll(url, table, template, fragment) {
         console.log(error)
         table.textContent = message
     }
+}
+
+function setTextContent(el,template) {
+    template.querySelector(".item-name").textContent = el.name;
+    template.querySelector(".item-price").textContent = el.price;
+    template.querySelector(".item-category").textContent = el.category;
+    template.querySelector(".item-description").textContent = el.description;
+    // DANDOLE EL SRC Y ALT A LAS IMAGENES (SI ES QUE EXISTEN)
+    if (el.imagen.path) {
+        template.querySelector(".item-image__img").src = `../${el.imagen.path}`
+        template.querySelector(".item-image__img").alt = el.imagen.originalname;
+    } else {
+        template.querySelector(".item-image__img").src = ""
+        template.querySelector(".item-image__img").alt = "sin imagen"
+    }
+}
+
+function setDataset(el, template) {
+    template.querySelector(".edit-btn").dataset._id = el._id;
+    template.querySelector(".item-tr-fetch").dataset._id = el._id;
+    template.querySelector(".item-tr-fetch").dataset.name = el.name;
+    template.querySelector(".item-tr-fetch").dataset.price = el.price;
+    template.querySelector(".item-tr-fetch").dataset.category = el.category;
+    template.querySelector(".item-tr-fetch").dataset.description = el.description;
+    template.querySelector(".delete-btn").dataset._id = el._id;
+}
+
+function cloneTemplate(fragment, template) {
+    const clone = document.importNode(template, true);
+    fragment.appendChild(clone);
 }
