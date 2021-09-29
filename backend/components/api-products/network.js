1ecb8be26e759803fb90e372649238dfee851bb1
Andre del Alcazar <adac9525@gmail.com>
2021-09-29 17:43:41 -0500

    protegiendo con middlewears la ruta de admin en adelante

--- backend/components/api-products/network.js
+++ backend/components/api-products/network.js
@@ -17,9 +17,9 @@ router.get('/', (req, res) => {
 })
 
 router.post('/',
-[authJwt.isAuthenticated
-    // ,authJwt.isAdmin
-    , passport.authenticate('jwt', { session: false })],
+    [authJwt.isAuthenticated
+        , authJwt.isAdmin
+        , passport.authenticate('jwt', { session: false })],
     (req, res) => {
         const { name, price, category, description } = req.body
         controller.addProduct(name, price, category, description, req.file)
@@ -32,9 +32,9 @@ router.post('/',
     })
 
 router.patch('/:id',
-[authJwt.isAuthenticated
-    // ,authJwt.isAdmin
-    , passport.authenticate('jwt', { session: false })],
+    [authJwt.isAuthenticated
+        ,authJwt.isAdmin
+        , passport.authenticate('jwt', { session: false })],
     (req, res) => {
         const { name, price, category, description } = req.body
         controller.patchProduct(req.params.id, name, price, category, description)
@@ -47,7 +47,7 @@ router.patch('/:id',
     })
 
 router.delete('/:id',
-    [authJwt.isAuthenticated,authJwt.isAdmin, passport.authenticate('jwt', { session: false })],
+    [authJwt.isAuthenticated, authJwt.isAdmin, passport.authenticate('jwt', { session: false })],
     (req, res) => {
         controller.deleteProduct(req.params.id).
             then(() => {
--- backend/components/network/admin_views.js
+++ backend/components/network/admin_views.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router()
 const authJwt = require('../middlewares/auth')
 
-router.get('/', (req, res) => {
+router.get('/', authJwt.isAuthenticated, authJwt.isAdmin, (req, res) => {
     res.render('admin')
 })
 
-router.get('/create-products', authJwt.isAuthenticated, async (req, res) => {
+router.get('/create-products', authJwt.isAuthenticated, authJwt.isAdmin, async (req, res) => {
     res.render('create-products')
 })
 
